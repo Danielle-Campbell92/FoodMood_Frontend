5c1ad1565d13e4fb0f89ed9b549ba48ffd310fc2
Danielle <dani.camp1125@gmail.com>
2025-07-03 19:44:45 -0400

    Adding Navigations and token to app.jsx--Dani

--- App.jsx
+++ App.jsx
@@ -1,8 +1,28 @@
-export default function App(){
+import Navigations from './components/Navigations';
+import RecipeList from './compnenents/RecipeList';
+import RecipeDetails from './componenets/RecipeDetails';
+import 
+
+
+function App(){
+    const [token, setToken] = useState(localStorage.getItem("token"))
+
+
     return(
         <>
         <div>
+            <Navigations token={token} setToken={setToken}/>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/saved" element={<Saved token={token} setToken={setToken}/>}/>
+                <Route path="/login" element={<Login token={token} setToken={setToken}/>}/>
+                <Route path="/register" element={<Register token={token} setToken={setToken}/>}/>
+                <Route path="/recipes" element={<RecipeList/>}/>
+                <Route path="/recipes/:id" element={<RecipeDetails/>}/>
+            </Routes>
         </div>
         </>
-    );
+    )
 }
+
+export default App;
