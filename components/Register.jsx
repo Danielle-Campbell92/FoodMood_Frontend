78c5941a86c49e14f7884aa50c603169b96ac790
Danielle <dani.camp1125@gmail.com>
2025-07-03 16:14:57 -0400

    set up imports and try catch framework for handleSubmit --Dani

--- components/Register.jsx
+++ components/Register.jsx
@@ -1,7 +1,22 @@
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
+export default function Register({token, setToken, username, setUsername, password, setPassword}){
+    const [error, setError] = useState(null)
+    const navigate = useNavigate()
+    
+    async function handleSubmit(e){
+        e.preventDefault()
 
-export default function Register(){
+        try{
+
+        }catch(error){
+            setError("Unable to register")
+        }
+
+    }
     return(
         <>
         </>
