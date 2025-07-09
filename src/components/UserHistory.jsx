a712fda90a54740f47deb602575eb960e48d2fbb
Danielle <dani.camp1125@gmail.com>
2025-07-08 20:49:06 -0400

    created useState to rerender the array for moods--Dani

--- src/components/UserHistory.jsx
+++ src/components/UserHistory.jsx
@@ -2,9 +2,15 @@ import { useEffect, useState } from 'react';
 import { Doughnut } from 'react-chartjs-2';
 
 export default function UserHistory(){
+    const [moodData, setMoodData] = useState([])
+
+
 
     
     return(
-        <></>
+        <>
+        <div>
+        <h2>Your Mood History</h2>
+        </div></>
     )
 }
