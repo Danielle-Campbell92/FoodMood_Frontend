4d620255fa5bd9e2363725e0f0063ccfa409bfac
Lyndsey <lyndseymsmith44@gmail.com>
2025-07-08 20:54:29 -0400

    Completed Login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Navigations from './components/Navigations.jsx';
-import RecipeDetails from './components/RecipeDetails.jsx';
-import RecipeList from './components/RecipeList.jsx';
+// import RecipeDetails from './components/RecipeDetails.jsx';
+// import RecipeList from './components/RecipeList.jsx';
 import {Routes, Route} from 'react-router-dom';
-import Register from './components/Register.jsx';
-import Login from './components/Login.jsx';
+// import Register from './components/Register.jsx';
+// import Login from './components/Login.jsx';
 import UserHistory from './components/UserHistory.jsx';
 import MoodHome from './components/MoodHome.jsx';
 
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,67 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
+export default function Login({
+  username,
+  setUsername,
+  password,
+  setPassword,
+  token,
+  setToken,
+}) {
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+  const navigate = useNavigate();
 
-export default function Login(){
-    return(
-        <>
-        </>
-    )
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      const responst = await fetch("https://localhost:3000/api/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setToken(result.token);
+        localStorage.setItem("token", result.token);
+        setUsername(result.username);
+        setSuccess("Successfully logged in");
+        navigate("/");
+      } else {
+        const errorData = await response.json();
+        alert(errorData.error || "Login failed");
+      }
+    } catch (error) {
+      setError("Unable to login");
+      alert("Unable to login");
+    }
+  }
+
+  return (
+  <div>
+    <h2>Login to your account below:</h2>
+    <form onSubmit={handleSubmit}>
+        <label>
+            Username: 
+            <input value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <br />
+        <br />
+        <label>
+            Password:
+            <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <br />
+        <br />
+        <button type="submit">Login</button>
+    </form>
+  </div>)
 }
