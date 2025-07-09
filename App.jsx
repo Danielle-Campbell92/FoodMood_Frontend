fde8f4d28f879da3ace2360fcf5dca44408e5b0c
Danielle <dani.camp1125@gmail.com>
2025-07-08 20:01:40 -0400

    Added imports to connect to Route--Dani

--- App.jsx
+++ App.jsx
@@ -1,7 +1,11 @@
 import Navigations from './components/Navigations';
 import RecipeList from './compnenents/RecipeList';
 import RecipeDetails from './componenets/RecipeDetails';
-import {Routes, Route} from ''
+import {Routes, Route} from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+import UserHistory from './components/UserHistory';
+import MoodHome from './components/MoodHome';
 
 
 export default function App(){
@@ -13,8 +17,8 @@ export default function App(){
         <div>
             <Navigations token={token} setToken={setToken}/>
             <Routes>
-                <Route path="/" element={<Home />}/>
-                <Route path="/saved" element={<Saved token={token} setToken={setToken}/>}/>
+                <Route path="/" element={<MoodHome />}/>
+                <Route path="/userhistory" element={<UserHistory token={token} setToken={setToken}/>}/>
                 <Route path="/login" element={<Login token={token} setToken={setToken}/>}/>
                 <Route path="/register" element={<Register token={token} setToken={setToken}/>}/>
                 <Route path="/recipes" element={<RecipeList/>}/>
