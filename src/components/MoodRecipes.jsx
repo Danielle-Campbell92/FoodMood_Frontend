1e9aef35829471f983223ebff8af1fd5ade1da4b
Danielle <dani.camp1125@gmail.com>
2025-07-13 14:16:12 -0400

    changed params to mood instead of id

--- src/components/MoodRecipes.jsx
+++ src/components/MoodRecipes.jsx
@@ -1,26 +1,33 @@
 import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import RecipeDetails from './RecipeDetails';
 
 
 export function MoodRecipes(){
-    const { id } = useParams();
+    const { mood } = useParams();
     const [recipes, setRecipes] = useState([])
     const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchMoodRecipes = async () => {
             try{
-                const response = await fetch('')
+                const response = await fetch(`http://localhost:3000/recipes/${mood}`)
                 const result = await response.json()
                 setRecipes(result)
             }catch(error){
             setError(error)} 
         }
         fetchMoodRecipes()
-    }, [id])
+    }, [mood])
 
     return(
         <>
+        <div>
+            <h2>Recipes for Mood: {mood}</h2>
+            {recipes.map((recipe) => (
+                <RecipeDetails key={recipe.id} {...recipe}/>
+            ))}
+        </div>
         </>
     )
 }
