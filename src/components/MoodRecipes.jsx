5a06bfb0ce163a2d12f47d7e53b282ad5c890bb0
Danielle <dani.camp1125@gmail.com>
2025-07-13 13:55:43 -0400

    set up params and necessary useState

--- src/components/MoodRecipes.jsx
+++ src/components/MoodRecipes.jsx
@@ -1,4 +1,11 @@
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react-router-dom';
+
+
 export function MoodRecipes(){
+    const { id } = useParams();
+    const [recipes, setRecipes] = useState([])
+    const [error, setError] = useState(null);
 
     return(
         <>
