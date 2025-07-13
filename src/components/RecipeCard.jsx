39a9a0f45c621201cf9db5e8ea9ac11a2715837b
Danielle <dani.camp1125@gmail.com>
2025-07-13 15:12:12 -0400

    created simple props and recipe card to get title and descrition

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-export default function RecipeCard(){
+export default function RecipeCard({title, description}){
 
     return(
         <>
+        <div>
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </div>
         </>
     )
 }
