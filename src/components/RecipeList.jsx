34e8eeb189735b750b996121bc2eb0afc8c634a0
Danielle <dani.camp1125@gmail.com>
2025-07-13 13:53:10 -0400

    commented out code temporarily

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,44 +1,44 @@
-import RecipeDetails from "./RecipeDetails";
-import { useState, useEffect } from "react";
+// import RecipeDetails from "./RecipeDetails";
+// import { useState, useEffect } from "react";
 
-export default function RecipeList({ recipe }) {
-  const [recipes, setRecipes] = useState([]);
-  const [error, setError] = useState(null);
+// export default function RecipeList({ recipe }) {
+//   const [recipes, setRecipes] = useState([]);
+//   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    async function fetchAllRecipes() {
-      try {
-        const response = await fetch("http://localhost:3000/recipes");
+//   useEffect(() => {
+//     async function fetchAllRecipes() {
+//       try {
+//         const response = await fetch("http://localhost:3000/recipes");
 
-        const data = await response.json();
-        console.log(data);
+//         const data = await response.json();
+//         console.log(data);
 
-        setRecipes(data);
-      } catch (err) {
-        setError("Unable to fetch recipes");
-        console.error(err);
-      }
-    }
-    fetchAllRecipes();
-  }, []);
+//         setRecipes(data);
+//       } catch (err) {
+//         setError("Unable to fetch recipes");
+//         console.error(err);
+//       }
+//     }
+//     fetchAllRecipes();
+//   }, []);
 
-  if (error) {
-    return <p>Error: {error}</p>;
-  }
+//   if (error) {
+//     return <p>Error: {error}</p>;
+//   }
 
-  return (
-    <div>
-      {recipes.map((recipe) => (
-        <RecipeDetails
-          key={recipe.id}
-          id={recipe.id}
-          title={recipe.title}
-          description={recipe.description}
-          ingredients={recipe.ingredients}
-          instructions={recipe.instructions}
-          mood_id={recipe.mood_id}
-        />
-      ))}
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       {recipes.map((recipe) => (
+//         <RecipeDetails
+//           key={recipe.id}
+//           id={recipe.id}
+//           title={recipe.title}
+//           description={recipe.description}
+//           ingredients={recipe.ingredients}
+//           instructions={recipe.instructions}
+//           mood_id={recipe.mood_id}
+//         />
+//       ))}
+//     </div>
+//   );
+// }
