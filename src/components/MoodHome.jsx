0f3eb8950323f2dd6a937f35ef559ea04c05d524
Rebekah Gibson <blgibson2001@gmail.com>
2025-07-14 12:25:40 -0500

    starter styling to mood/home page, still in progress

--- src/components/MoodHome.jsx
+++ src/components/MoodHome.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect} from "react"
 import {Link} from "react-router-dom"
 import Navigations from "./Navigations"
 import React from "react"
+import '../App.css'
 
 const MoodsHome = () =>{
     const [moods, setMoods] = useState([]);
@@ -22,16 +23,18 @@ const MoodsHome = () =>{
         fetchMoods();
     }, []);
 
-    return( 
-        <>
+    return(
+        <div className='mood-button-container'>
+            <h2>Hello and welcome to FoodMood! To continue, select the button that best aligns with your current mood.</h2>
+                <h3>Hint: Login to save your favorite recipes and playlists!</h3>
         {moods.map((mood) => (
         <Link key={mood.id} to={`/mood/${mood.emotion}`}>
-            <button >
+            <button className={`card card-${mood.emotion.toLowerCase()}`}>
                 <strong>{mood.emotion}</strong>: {mood.description}
             </button>
         </Link>
       ))}
-        </>
+        </div>
     )
 }
 
