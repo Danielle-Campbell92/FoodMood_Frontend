11bb4215332de47f7946acde3fb95cc55b16021e
Danielle <dani.camp1125@gmail.com>
2025-07-15 19:41:57 -0400

    added react to be imported

--- src/components/RecipeDetails.jsx
+++ src/components/RecipeDetails.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import React from 'react';
 
 export default function RecipeDetails() {
   const [recipe, setRecipe] = useState(null);
